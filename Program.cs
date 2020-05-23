66dccb945ebee9d15d73450440fa72215591695a
Phxmous <wowmodz@live.com>
2020-05-22 23:21:49 -0400

    init.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Thesaurus_Web_Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
