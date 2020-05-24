3efbd5ed86703fc30941661edd8d521d4f771a4e
Phxmous <wowmodz@live.com>
2020-05-24 06:07:48 -0400

    reads and filters entries. TODO: - Write nouns/adjectives to file. - Optimizations by separating each section into its own threads.

--- Program.cs
+++ Program.cs
@@ -2,45 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using HtmlAgilityPack;
 
 class Program
 {
     private const string directory = "https://www.merriam-webster.com/browse/thesaurus/";
+    private const string entryDirectory = "https://www.merriam-webster.com/thesaurus/";
 
-    private static HttpClient httpClient = new HttpClient();
-    private static HtmlDocument document = new HtmlDocument();
-    private static List<HtmlNode> wordNodes = new List<HtmlNode>();
+    private static List<String> pageEntries = new List<String>();
+
+    private static List<string> nouns = new List<string>();
+    private static List<string> adjectives = new List<string>();
 
     private static void Main(string[] args)
     {
-        StartCrawlerSync();
+        StartRootCrawler();
         Console.ReadKey();
     }
 
-    private static async void StartCrawlerSync()
+    private static async void StartRootCrawler()
     {
         //Loop through ascii characters
         for (int i = 97; i <= 122; i++)
         {
-            string targetURL = directory + (char) i;
-            string html = await httpClient.GetStringAsync(targetURL);
-            document.LoadHtml(html);
-            int pageCount = GetPageCount();
-            for (int j = 0; j <= pageCount; j++)
+            string rootURL = directory + (char) i;
+            HtmlDocument mainDocument = new HtmlDocument();
+            HttpClient rootClient = new HttpClient();
+            mainDocument.LoadHtml(await rootClient.GetStringAsync(rootURL));
+            int pageCount = GetPageCount(mainDocument);
+            for (int j = 1; j <= pageCount; j++)
             {
-                
+                string countedURL = rootURL + '/' + j.ToString();
+                HtmlDocument pageDocument = new HtmlDocument();
+                HttpClient pageClient = new HttpClient();
+                pageDocument.LoadHtml(await pageClient.GetStringAsync(countedURL));
+                PopulatePageEntries(pageDocument);
+                pageClient.Dispose();
+                foreach (string entry in pageEntries)
+                {
+                    HtmlDocument entryDocument = new HtmlDocument();
+                    HttpClient entryClient = new HttpClient();
+                    entryDocument.LoadHtml(await entryClient.GetStringAsync(entryDirectory + entry));
+                    FilterEntry(entry, entryDocument);
+                    entryClient.Dispose();
+                }
+                Console.WriteLine($"Page {j} complete for {(char)i}");
+            }
+            Console.WriteLine($"Nouns added: {nouns.Count}");
+            Console.WriteLine($"Adjectives added: {adjectives.Count}");
+            rootClient.Dispose();
+        }
+    }
+
+    private static int GetPageCount(HtmlDocument document)
+    {
+        return Convert.ToInt32(document.DocumentNode.Descendants("span").Where(node => node.GetAttributeValue("class", "").Equals("counters")).FirstOrDefault().InnerText.Split("of ") [1]);
+    }
+
+    private static void PopulatePageEntries(HtmlDocument document)
+    {
+        List<HtmlNode> entryParentNodes = new List<HtmlNode>(document.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("entries")).ToList());
+        foreach (HtmlNode parentNode in entryParentNodes)
+        {
+            List<HtmlNode> entryNodes = new List<HtmlNode>(parentNode.Descendants("a").ToList());
+            foreach (HtmlNode entryNode in entryNodes)
+            {
+                pageEntries.Add(entryNode.InnerText);
             }
         }
     }
 
-    private static int GetPageCount()
+    private static void FilterEntry(string entry, HtmlDocument document)
     {
-        return Convert.ToInt32(document.DocumentNode.Descendants("span").Where(node => node.GetAttributeValue("class", "").Equals("counters")).FirstOrDefault().InnerText.Split("of ")[1]);
-    }
-
-    private static List<HtmlNode> GetPageEntries()
-    {
-        return document.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("entries")).ToList();
+        try
+        {
+            string lexicalCategory = document.DocumentNode.Descendants("a").Where(node => node.GetAttributeValue("class", "").Equals("important-blue-link")).FirstOrDefault().InnerText.ToLower();
+            if (lexicalCategory == "noun") { nouns.Add(entry); }
+            else if (lexicalCategory == "adjective" || lexicalCategory == "adj") { adjectives.Add(entry); }
+        }
+        catch
+        {
+            return;
+        }
     }
 }
